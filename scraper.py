c69a8894706d2a77ca1a56bcd559375d4396e055
Jeffrey <jeffrey@wirehead.be>
2021-11-02 10:20:20 +0100

    initial push

---
+++ scraper.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+# add local libraries
+# mkdir lib
+# pip3 install pandas bs4 -t lib/
+
+# import local libraries
+import sys
+sys.path.append('/home/jeffrey/source/spotscraper/lib')
+
+# import scraper libs
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+# import time lib
+import datetime
+
+# import json lib
+import json
+
+# get dates
+today = datetime.date.today()
+tomorrow = datetime.date.today() + datetime.timedelta(days=1)
+
+d1 = today.strftime("%Y-%m-%d")
+d2 = tomorrow.strftime("%Y-%m-%d")
+
+# set epexspot URL
+url='https://www.epexspot.com/en/market-data?market_area=BE&trading_date=' + d1 + '&delivery_date=' + d2 + '&underlying_year=&modality=Auction&sub_modality=DayAhead&product=60&data_mode=table&period='
+
+# get url
+r = requests.get(url)
+html = r.text
+soup = BeautifulSoup(html, "html.parser")
+
+# find the wanted table
+table = soup.find('table', {"class": "table-01 table-length-1"})
+# select data to filter
+rows = table.find_all('tr')
+
+data = []
+for row in rows[6:]: #suppress first 6 lines
+    cols = row.find_all('td')
+    cols = [ele.text.strip() for ele in cols]
+    data.append([ele for ele in cols if ele])
+
+# print the data
+result = pd.DataFrame(data, columns=['Buy Volume', 'Sell Volume', 'Volume', 'Price'])
+result.reset_index(inplace=False)
+result.drop('Buy Volume', inplace=True, axis=1)
+result.drop('Sell Volume', inplace=True, axis=1)
+result.drop('Volume', inplace=True, axis=1)
+#print(result)
+
+resultjson = result.to_json(orient="split")
+parsed = json.loads(resultjson)
+print(json.dumps(parsed, indent=4))
